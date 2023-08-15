4abaf70fffc45203d23c2a289494e31d58588298
Wasif007 <wasifateeq007@hotmail.com>
2023-08-15 23:03:34 +0500

    styling using chakra UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
-
+import { Box,VStack,Container, Button, Input, HStack } from "@chakra-ui/react";
 function App() {
   return (
-  <div>
-    Hello
-    </div>
+  <Box bg={"red.100"}>
+    <Container h={"100vh"} bg={"white"}>
+     <VStack h={"full"} bg={"blue.100"}>
+      <Button w={"full"} colorScheme={"red"}>Sign Out</Button>
+      <VStack width={"full"} height={"full"} bg={"purple.100"}>
+
+      </VStack>
+      <form>
+        <HStack>
+          <Input bg={"white"}/>
+          <Button type="submit" colorScheme={"purple"}>Send</Button>
+        </HStack>
+      </form>
+      
+     </VStack>
+    </Container>
+  </Box>
   );
 }
 
